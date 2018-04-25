fea95f983ff065bc2748b0dbbd96c7f7e263451b
SteGriff <github@stegriff.co.uk>
2018-04-25 10:48:06 +0100

    Work on plaintext api

--- api/v2.php
+++ api/v2.php
@@ -3,7 +3,6 @@
 require '../ext.php';
 require '../DAL.php';
 require '../views.php';
-require '../wordnik-php/wordnik/Swagger.php';
 
 error_reporting(E_ALL);
 
@@ -11,12 +10,22 @@ function sendHeader($format)
 {
 	switch ($format)
 	{
+		case 'plain':
+			header("content-type: text/plain");
+			break;
 		case 'html':
 		default:
-
+			header("content-type: text/html");
 			echo "<html><head><meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\"></head><body>";
-			// break;
+			break;
 	}
+}
+
+function parseContentType($acceptHeader)
+{
+	$acceptHeader = strtolower(trim($acceptHeader));
+	$acceptHeader = str_ireplace(['text', 'application', '/'], '', $acceptHeader); 
+	return $acceptHeader;
 }
 
 //Get request stuff
@@ -27,11 +36,13 @@ if ($word === null){
 
 //Spoof the accept header
 // (until we support more formats)
-$format = 'html';
+$acceptHeader = isset($_SERVER['Accept']) ? $_SERVER['Accept'] : 'text/html';
+$format = parseContentType($acceptHeader);
 
 sendHeader($format);
 
 //Set up client
+require '../wordnik-php/wordnik/Swagger.php';
 $myAPIKey = 'ac136a0a065b4ab991c0f08611306bf6e9efa80027bd69a23';
 $client = new APIClient($myAPIKey, 'http://api.wordnik.com/v4');
 $wordApi = new WordApi($client);
@@ -46,7 +57,7 @@ $definitions = getDefinitions($word);
 $etyms = getEtymologies($word);
 
 //Generate view
-$view .= "<h2>$word</h2>";
+$view .= viewTitle($word, $format);
 $view .= viewDefinitions($definitions, $format);
 $view .= viewEtymologies($etyms, $format);
 
--- views.php
+++ views.php
@@ -6,6 +6,8 @@ function dictionaryAttribution($dictionaryCode, $type)
 	
 	switch ($type)
 	{
+		case 'plain':
+			return $friendlyName;
 		case 'html':
 		default:
 			return "<h3>$friendlyName</h3>";
@@ -21,6 +23,8 @@ function dictionaryFriendlyName($dictionaryCode)
 			return "American Heritage Dictionary";
 		case 'wiktionary':
 			return "Wiktionary";
+		default:
+			return '';
 	}
 }
 
@@ -36,21 +40,26 @@ function viewDefinitions($defs, $type)
 	
 	switch ($type)
 	{
+		case 'plain':
+			foreach ($defs as $d)
+			{
+				$response .= "{$d->partOfSpeech}: {$d->text}\r\n";
+			}
+			break;
 		case 'html':
-			$response = '';
 			foreach ($defs as $d)
 			{			
 				if ($lastSource === '')
 				{
 					//First dictionary
 					$lastSource = $d->sourceDictionary;
-					$response .= dictionaryAttribution($lastSource, 'html') . " <dl>";
+					$response .= dictionaryAttribution($lastSource, $type) . " <dl>";
 				}
 				elseif ($lastSource != $d->sourceDictionary)
 				{
 					//New dictionary
 					$lastSource = $d->sourceDictionary;
-					$response .= "</dl> " . dictionaryAttribution($lastSource, 'html') . " <dl>";
+					$response .= "</dl> " . dictionaryAttribution($lastSource, $type) . " <dl>";
 				}
 				//Else: same dictionary (do nothing)
 				
@@ -83,6 +92,29 @@ function viewEtymologies($etyms, $type)
 				$response .= "<li>$e</li>";
 			}
 			$response .= '</ul>';
+			break;
+	}
+	return $response;
+}
+
+function viewTitle($title, $type)
+{
+	$response = '';
+	
+	switch ($type)
+	{
+		case 'html':
+			$response = '<ul>';			
+			foreach ($etyms as $e)
+			{
+				$e = str_replace("<?xml version=\"1.0\" encoding=\"UTF-8\"?>", '', $e);
+				$response .= "<li>$e</li>";
+			}
+			$response .= '</ul>';
+			break;
+		case 'plain':
+		default:
+			$response = "$title\r\n";
 			break;
 	}
 	return $response;
